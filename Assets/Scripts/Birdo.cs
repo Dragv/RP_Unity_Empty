fa332e5b7003bccb07cc1fc4d093b60e2ad54b79
Pedro <pedruxavelino@gmail.com>
2020-05-21 23:37:05 -0300

    Added Birdo Movement

---
+++ Assets/Scripts/Birdo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Birdo : MonoBehaviour
+{
+    public float _jumpForce;
+
+    Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.velocity = Vector2.zero;
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
