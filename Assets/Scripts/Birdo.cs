a0009295811c110fa1f3d8bc623998a6c008d104
Pedro <pedruxavelino@gmail.com>
2020-05-22 00:40:14 -0300

    Added UI Stuff Lives and Dash

--- Assets/Scripts/Birdo.cs
+++ Assets/Scripts/Birdo.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class Birdo : MonoBehaviour
 {
     public float _jumpForce;
 
     Rigidbody2D _rb;
+
+    public int _lives = 3;
+
+    public float _dashCooldown;
+    public float _currentCooldown;
+    public  bool _dead;
 
     private void Awake()
     {
@@ -13,10 +21,72 @@ public class Birdo : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!_dead)
         {
-            _rb.velocity = Vector2.zero;
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rb.velocity = Vector2.zero;
+                _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            }
+
+            //
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                //_lives--;
+
+
+                if (_currentCooldown >= _dashCooldown)
+                {
+                    StartCoroutine("Dash");
+                }
+            }
+
+            if (_currentCooldown < _dashCooldown)
+            {
+                _currentCooldown += 1f * Time.deltaTime;
+            }
         }
+    }
+
+    IEnumerator Dash()
+    {
+        _rb.velocity = Vector2.zero;
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+        _currentCooldown = 0;
+
+        yield return new WaitForSeconds(.3f);
+
+
+        _rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pipe")
+        {
+            HitPipe();
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            Die();
+        }
+    }
+
+    private void HitPipe()
+    {
+        if (_lives == 0)
+        {
+            Die();
+        }
+
+        GameManager.instance.ClearScreen();
+    }
+
+    private void Die()
+    {
+        //Do something i dunno
+        _lives = 0;
+        _dead = true;
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    public GameObject[] _lives;
+    public Birdo _birdo;
+
+    public bool _paused;
+
+    public Slider _slider;
+    public Image _fillArea;
+
+    private void Start()
+    {
+        for (int i = 0; i < _lives.Length; i++)
+        {
+            _lives[i].SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        _slider.value = _birdo._currentCooldown;
+
+        if (_slider.value >= 3)
+        {
+            _fillArea.color = Color.green;
+        }
+        else
+        {
+            _fillArea.color = Color.white;
+        }
+
+        switch (_birdo._lives)
+        {
+            case 2:
+                _lives[2].SetActive(false);
+                break;
+            case 1:
+                _lives[1].SetActive(false);
+                break;
+            case 0:
+                _lives[2].SetActive(false);
+                _lives[1].SetActive(false);
+                _lives[0].SetActive(false);
+                GameOver();
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    public void ClearScreen()
+    {
+
+    }
+
+    private void GameOver()
+    {
+        
+    }
+}
